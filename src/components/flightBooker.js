b4214ee281614e715d8e0c88ea0341f1ef4c7bb9
Emily Harber <emily.a.harber@gmail.com>
2020-06-25 14:01:41 -0400

    finished flight booker

--- src/components/flightBooker.js
+++ src/components/flightBooker.js
@@ -4,7 +4,7 @@ const FlightBooker = () => {
   const [flightType, setFlightType] = useState('one-way');
   const [flightOut, setFlightOut] = useState('');
   const [flightBack, setFlightBack] = useState('');
-  const [error, setError] = useState({ flightOut: false, returnFlight: false });
+  const [error, setError] = useState({ flightOut: false, flightBack: false });
   const flightOutRef = useRef();
   const returnFlightRef = useRef();
 
@@ -18,11 +18,11 @@ const FlightBooker = () => {
     }
   };
 
-  const dateRegExp = /^((0){1}[1-9]{1}|(1){1}[12]{1})-(([0-2]{1}[0-9]{1})|3[01])-20[\d]{2}$/g;
+  const dateRegExp = /^((0){1}[1-9]{1}|(1){1}[0-2]{1})-(([0]{1}[1-9]{1})|([12]{1}[0-9]{1})|3[01])-20[\d]{2}$/g;
 
   const changeFlightOut = (e) => {
     // use regex to match approved date format of MM-DD-YYYY
-    if (dateRegExp.test(e.target.value)) {
+    if (dateRegExp.test(e.target.value) || e.target.value === '') {
       setError((prev) => ({
         ...prev,
         flightOut: false,
@@ -34,23 +34,60 @@ const FlightBooker = () => {
         ...prev,
         flightOut: true,
       }));
+      setFlightOut(e.target.value);
     }
   };
 
   const changeReturnFlight = (e) => {
     // use regex to match approved date format of MM-DD-YYYY
-    // if it does not match, use 'flightOut' ref to highlight input as having an error, and setError to true
+    if (dateRegExp.test(e.target.value) || e.target.value === '') {
+      setError((prev) => ({
+        ...prev,
+        flightBack: false,
+      }));
+      setFlightBack(e.target.value);
+    } else {
+      // if it does not match, use 'flightOut' ref to highlight input as having an error, and setError to true
+      setError((prev) => ({
+        ...prev,
+        flightBack: true,
+      }));
+      setFlightBack(e.target.value);
+    }
   };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
     //check if error === true, and alert
-    //otherwise alert with flight details
+    //check flightType
+    if (flightType === 'one-way') {
+      if (error.flightOut || flightOut === '') {
+        alert(`Please choose a valid flight date.`);
+      } else {
+        alert(`Your flight has been booked for ${flightOut}`);
+        setFlightOut('');
+      }
+    } else {
+      if (error.flightOut || error.flightBack) {
+        alert(
+          `Please choose a valid date for your ${
+            error.flightOut ? 'flight out' : 'flight back'
+          }.`
+        );
+      } else if (flightOut === '' || flightBack === '') {
+        alert(`Please fill in your flight dates.`);
+      } else {
+        alert(`Your flight has been booked for ${flightOut} - ${flightBack}`);
+        setFlightOut('');
+        setFlightBack('');
+      }
+    }
   };
 
   return (
     <div className='box'>
       <p className='box-heading'>Flight Booker</p>
-      <form id='flight-booker'>
+      <form id='flight-booker' onSubmit={handleSubmit}>
         <select
           name='flight-type'
           value={flightType}
@@ -66,12 +103,16 @@ const FlightBooker = () => {
           ref={flightOutRef}
           onChange={changeFlightOut}
           className={error.flightOut ? 'error-field' : null}
+          value={flightOut}
         />
         <input
           type='text'
           id='flight-back'
           placeholder='06-25-2020'
           ref={returnFlightRef}
+          value={flightBack}
+          onChange={changeReturnFlight}
+          className={error.flightBack ? 'error-field' : null}
           disabled
         />
         <button type='submit'>Book Flight</button>
