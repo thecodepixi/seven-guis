ff3cff4119d2a6af98d23529d41ecdc47e843266
Emily Harber <emily.a.harber@gmail.com>
2020-06-28 22:13:14 -0400

    can adjust circle size! 🎉 need to figure out how to hide overflow and then handle undo/redo

--- src/components/circleDrawer.js
+++ src/components/circleDrawer.js
@@ -1,16 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 const SizeModal = React.forwardRef((props, modal) => {
+  const [radius, setRadius] = useState(10);
+
+  const handleChange = (e) => {
+    e.stopPropagation();
+    setRadius(e.target.value);
+    props.circle.style.height = radius * 2 + 'px';
+    props.circle.style.width = radius * 2 + 'px';
+  };
+
   return (
-    <div id='circle-adjustment' ref={modal} onClick={props.modalClicks}>
+    <div
+      id='circle-adjustment'
+      onClick={props.modalClicks}
+      ref={modal}
+      style={{
+        top: props.coords[1] + 10 + 'px',
+        left: props.coords[0] + 10 + 'px',
+      }}
+    >
       <p>Adjust Diameter</p>
-      <input type='range' max='100' />
+      <input type='range' max='100' value={radius} onChange={handleChange} />
     </div>
   );
 });
 
 const CircleDrawer = () => {
   const [showModal, setShowModal] = useState(false);
+  const [currentX, setCurrentX] = useState(0);
+  const [currentY, setCurrentY] = useState(0);
+  const [currentCircle, setCurrentCircle] = useState(null);
   const canvas = useRef();
   const modal = useRef();
 
@@ -24,17 +44,22 @@ const CircleDrawer = () => {
       return;
     } else {
       e.stopPropagation();
-      let x = e.pageX - 10;
-      let y = e.pageY - 10;
+      let x = e.pageX;
+      setCurrentX(x);
+      let y = e.pageY;
+      setCurrentY(y);
 
       let circ = document.createElement('div');
       circ.className = 'circ';
-      circ.style.left = x + 'px';
-      circ.style.top = y + 'px';
+      circ.style.left = x - 10 + 'px';
+      circ.style.top = y - 10 + 'px';
+
       circ.addEventListener('click', (e) => {
         e.stopPropagation();
         setShowModal(true);
+        setCurrentCircle(circ);
       });
+
       canvas.current.appendChild(circ);
     }
   };
@@ -47,7 +72,14 @@ const CircleDrawer = () => {
         <button>Redo</button>
       </div>
       <div id='circle-canvas' ref={canvas} onClick={drawCircle}>
-        {showModal ? <SizeModal showModal={setShowModal} ref={modal} /> : null}
+        {showModal ? (
+          <SizeModal
+            showModal={setShowModal}
+            ref={modal}
+            coords={[currentX, currentY]}
+            circle={currentCircle}
+          />
+        ) : null}
       </div>
     </div>
   );
